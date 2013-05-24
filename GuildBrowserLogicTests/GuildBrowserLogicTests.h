b160c3c66004083b82e61f451242dd61936e4095
Adrian O'Sullivan <adrianosullivan@Adrians-Mac-mini.local>
2013-05-24 10:49:35 +0100

    commit after unit testing 24may

---
+++ GuildBrowserLogicTests/GuildBrowserLogicTests.h
@@ -0,0 +1,13 @@
+//
+//  GuildBrowserLogicTests.h
+//  GuildBrowserLogicTests
+//
+//  Created by Adrian O'Sullivan on 23/05/2013.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GuildBrowserLogicTests : SenTestCase
+
+@end
---
+++ GuildBrowserLogicTests/WowUtilsTest.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTest.h
+//  GuildBrowser
+//
+//  Created by Adrian O'Sullivan on 24/05/2013.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTest : SenTestCase
+
+@end
