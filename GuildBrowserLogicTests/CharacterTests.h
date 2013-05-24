6532aef382021818ee59ba11958b4695c598a2fd
Adrian O'Sullivan <adrianosullivan@Adrians-Mac-mini.local>
2013-05-24 11:44:20 +0100

    addded test target

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Adrian O'Sullivan on 24/05/2013.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
